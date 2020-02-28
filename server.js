a40a4ee4225f123fc39b54252f0e43ee562960ff
fehmivelioglu <fehmi_ali@hotmail.com>
2020-02-29 00:28:22 +0300

    nineth

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
-app.use(express.static(__dirname + "/dist/todo"));
+app.use(express.static(__dirname + "/dist/to-do"));
 app.listen(process.env.PORT || 8080);
 
 app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname + "/dist/todo/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/to-do/index.html"));
 })
 
 console.log("console listening");
